470779d11dc052fa053853b59f2873d8ae5b833c
ekaterinakibis <ek.kibis@gmail.com>
2022-07-14 15:28:36 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main  (String[] args){
+        System.out.println("Ekaterina Kibis");
+        System.out.println("15.03.1988");
+    }
+}
