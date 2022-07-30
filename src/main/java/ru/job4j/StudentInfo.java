6c8b7b9d001bf83816127012a7ac0bd0f7c83fd4
ekaterinakibis <ek.kibis@gmail.com>
2022-07-30 14:04:57 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
 
-    public static void main  (String[] args){
+    public static void main(String[] args) {
         System.out.println("Ekaterina Kibis");
         System.out.println("15.03.1988");
     }
