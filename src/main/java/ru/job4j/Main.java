46da3ddde86f65ae439e93c1b91a56eb1b79c520
ekaterinakibis <ek.kibis@gmail.com>
2022-10-24 13:03:26 +0500

    Арифметические операции. [#185 #331022]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,31 @@
 package ru.job4j;
 
+import java.util.Stack;
+
+//
+/*
+Create a util method to reverse string and delete all commas.
+"a,,c,,,d,,," -> "dca"
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args)
+    {
+        System.out.println(util("a,,c,,,d,,,"));
     }
+
+    private static String util(String input) {
+        if (input != null || input.length() > 0) return "";
+        StringBuilder result = new StringBuilder();
+            Stack<String> stack = new Stack<>();
+            for (String element : input.split("")) {
+                if (!",".equals(element)) {
+                    stack.push(element);
+                }
+            }
+            while (!stack.empty()) {
+                result.append(stack.pop());
+            }
+         return  result.toString();
+    }
+
 }
